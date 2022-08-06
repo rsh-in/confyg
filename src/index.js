26d636d1eb16810ac2824b405dcf83e760621fa0
imroshin <imroshin@gmail.com>
2022-08-07 00:11:30 +0530

    adding vuetify

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+// import Web3 from 'Web3';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+
+import App from './App.vue';
+
+new Vue({ render: createElement => createElement(App) }).$mount('#app');
