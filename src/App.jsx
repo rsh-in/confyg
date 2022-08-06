7d38f8d1c9e11d1aa9aaef111f78387ddb653002
imroshin <imroshin@gmail.com>
2022-08-06 14:41:12 +0530

    input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,36 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-  
-function App({
-  address,
-  userData,
-  avatarSrc,
-}) {
-  const router = useRouter();
+import { HeadProvider } from 'react-head';
+import {useImage} from 'react-image'
+import { Link } from "react-router-dom";
 
-  const { name, username, email, website, bio } = props.userData;
+import { Input } from '@web3uikit/core';
+
+import "/home/roshin/PROJECTS/confyg/styles/globals.css";
+import "/home/roshin/PROJECTS/confyg/styles/users.css";
+
+
+function App()
+{
+
+  // const { name, username, email, website, bio } = "";
+  // const name = 'roshin';
+  // const username = 'r0x';
+  // const email = 'roshin@gmail.com';
+  // const website = 'roninn.com';
+  // const bio = 'blahhhhh';
+
 
   return (
-    <div>
-      <Head>
-        <title>Privy Demo - User {props.address}</title>
-        <meta name="description" content="Privy user profile demo" />
-      </Head>
-
-      <main>
-        <header>
-          <h1>Privy Demo</h1>
-          <nav>
-            <Link href="/">Home</Link>
-            <SignOutLink />
-            <Link href="https://github.com/privy-io/privy-pii-demo">
-              View on Github
-            </Link>
-          </nav>
-        </header>
-
-        <div className="page-header">
-          <div className="avatar">
-            {props.avatarSrc != null ? (
-              <Image
-                src={props.avatarSrc}
-                alt="User avatar"
-                width={100}
-                height={100}
-              />
-            ) : (
-              <Image
-                src={avatarPlaceholder}
-                alt="Avatar placeholder"
-                width={100}
-                height={100}
-                quality={90}
-              />
-            )}
-          </div>
-          <h2 className="title">User {formatDisplayAddress(props.address)}</h2>
-        </div>
-
-        <div>
-          <div className="privy-field-group">
-            <div className="privy-field">
-              <strong>Name</strong>
-              <p>{name ? name.text() : "N/A"}</p>
-            </div>
-            <div className="privy-field">
-              <strong>Username</strong>
-              <p>{username ? username.text() : "N/A"}</p>
-            </div>
-          </div>
-
-          <div className="privy-field-group">
-            <div className="privy-field">
-              <strong>Email</strong>
-              <p>{email ? email.text() : "N/A"}</p>
-            </div>
-            <div className="privy-field">
-              <strong>Website</strong>
-              <p>{website ? website.text() : "N/A"}</p>
-            </div>
-          </div>
-
-          <div className="privy-field-group">
-            <div className="privy-field-full">
-              <strong>Bio</strong>
-              <p>{bio ? bio.text() : "N/A"}</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="privy-field-group">
-          <button
-            className="button"
-            onClick={(e) => {
-              e.preventDefault();
-              router.push("/edit");
-            }}
-          >
-            Edit
-          </button>
-        </div>
-      </main>
-    </div>
+    <>
+      <Input
+        label = 'Name'
+        name='nameChange'
+        width='100%'
+        labelBgColor='#141d26'
+      />
+    </>
   );
 }
 
