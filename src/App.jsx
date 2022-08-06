d1b56996bbd5067e0f25b181ae3c312ec64accb0
imroshin <imroshin@gmail.com>
2022-08-06 15:03:22 +0530

    input fields

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { HeadProvider } from 'react-head';
 import {useImage} from 'react-image'
 import { Link } from "react-router-dom";
 
-import { Input } from '@web3uikit/core';
+import { Input, TextArea} from '@web3uikit/core';
 
 import "/home/roshin/PROJECTS/confyg/styles/globals.css";
 import "/home/roshin/PROJECTS/confyg/styles/users.css";
@@ -13,14 +13,6 @@ import "/home/roshin/PROJECTS/confyg/styles/users.css";
 
 function App()
 {
-
-  // const { name, username, email, website, bio } = "";
-  // const name = 'roshin';
-  // const username = 'r0x';
-  // const email = 'roshin@gmail.com';
-  // const website = 'roninn.com';
-  // const bio = 'blahhhhh';
-
 
   return (
     <>
@@ -30,6 +22,38 @@ function App()
         width='100%'
         labelBgColor='#141d26'
       />
+
+      <Input
+        label = 'username'
+        name='usernameChange'
+        width='100%'
+        labelBgColor='#141d26'
+      />
+
+      <Input
+        label = 'Email'
+        name='test email input'
+        width='100%'
+        labelBgColor='#141d26'
+        type='email'
+      />
+
+      <Input
+        label = 'website'
+        name='websiteChange'
+        width='100%'
+        labelBgColor='#141d26'
+      />
+
+      <TextArea
+        label="Bio"
+        name="Test TextArea Default"
+        onBlur={function noRefCheck(){}}
+        onChange={function noRefCheck(){}}
+        placeholder="Type here field"
+        value="Tell us something about yourself"
+      />
+
     </>
   );
 }
